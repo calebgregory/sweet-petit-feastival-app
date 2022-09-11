99e189c7ba7875e9bc7141696a2d93d0359b4eb3
Caleb Gregory <calebgregory@gmail.com>
2022-09-11 17:50:54 -0600

    adds cloudfront invalidation to deploy script

--- client/deploy.sh
+++ client/deploy.sh
@@ -10,4 +10,23 @@ else
   echo "(>'')> bucket already exists"
 fi
 
-aws s3 sync build "s3://${bucket_name}"
+aws s3 sync build "s3://${bucket_name}" --acl=public-read
+
+cloudfront_distribution_id="$(
+  aws cloudfront list-distributions \
+    | python -c "$(cat <<EOM
+import sys
+import json
+dists = json.load(sys.stdin)
+for dist in dists['DistributionList']['Items']:
+    origins = dist['Origins']['Items']
+    if any(['${bucket_name}' in origin['Id'] for origin in origins]):
+        print(dist['Id'])
+EOM
+  )"
+)"
+
+aws cloudfront create-invalidation \
+  --distribution-id "${cloudfront_distribution_id}" \
+  --paths '/*' \
+  | cat
